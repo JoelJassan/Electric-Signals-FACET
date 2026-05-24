19e342f6b872bb3c7ef267484a9da486ee9ed91c
Joel Jassan <joeljassan@hotmail.com>
2026-05-24 14:25:43 -0300

    ✨ feat: se añade modificación de parámetros sigma y mu para la señal normal (modificación de media y selectividad)

--- Tema 1/ejercicio 6a.py
+++ Tema 1/ejercicio 6a.py
@@ -17,6 +17,11 @@ n = 100000
 uniforme = np.random.rand(n)
 normal = np.random.randn(n)
 
+#modifico sigma y mu
+sigma = .5 #desviacion estandar
+mu = 5 #media
+normal_mod = mu + sigma*normal
+
 #visualización de graficas
 fig, ax = plt.subplots(1, 2)
 
@@ -25,6 +30,7 @@ ax[0].set_xlabel("Rango de valores")
 ax[0].set_ylabel("Densidad de probabilidad uniforme")
 #ax[1].set_title("Histograma")
 
+ax[1].hist(normal, bins=100, density=True, edgecolor='black', linewidth=.5)
 ax[1].hist(normal, bins=100, density=True, edgecolor='black', linewidth=.5)
 ax[1].set_xlabel("Rango de valores")
 ax[1].set_ylabel("Densidad de probabilidad normal")
