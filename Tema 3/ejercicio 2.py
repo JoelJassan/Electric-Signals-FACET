396b10833d216724d0d07fc5e0d6fe1ff2d49d14
Joel Jassan <joeljassan@hotmail.com>
2026-06-02 17:06:34 -0300

    ✨ feat: se corrige funcionalidad y se añade señal 2

--- Tema 3/ejercicio 2.py
+++ Tema 3/ejercicio 2.py
@@ -10,32 +10,41 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Parámetros de la señal
-ciclos = 1
+ciclos = 2
 T = 1  # Período
 D = 0.5  # Ciclo activo del 50%
 N1 = 100  # Número de muestras por período (caso 1)
 N2 = 1000  # Número de muestras por período (caso 2)
 
 #muestreo
-f_sampling_1 = 1/T*N1
-f_sampling_2 = 1/T*N2
+f_sampling_1 = N1
+f_sampling_2 = N2
 
-sampling_time_1 = np.arange(0, ciclos/f_sampling_1, 1/f_sampling_1)
-sampling_time_2 = np.arange(0, ciclos/f_sampling_2, 1/f_sampling_2)
+sampling_time_1 = np.arange(0, ciclos/T, 1/f_sampling_1)
+sampling_time_2 = np.arange(0, ciclos/T, 1/f_sampling_2)
 
 
 # Función para generar la señal discreta
-square_wave = []
-for i in range(T):
-    if i < D*T:
-        square_wave.append(1)
+square_wave_1 = []
+for i in sampling_time_1:
+    if i%T < D*T:
+        square_wave_1.append(1)
     else:
-        square_wave.append(0)
-square_wave = np.array(square_wave)
+        square_wave_1.append(0)
+square_wave_1 = np.array(square_wave_1)
+
+square_wave_2 = []
+for i in sampling_time_2:
+    if i%T < D*T:
+        square_wave_2.append(1)
+    else:
+        square_wave_2.append(0)
+square_wave_1 = np.array(square_wave_1)
+
 
 #visualización de señales
-plt.plot(sampling_time_1, square_wave, label='N=100')
-plt.plot(sampling_time_2, square_wave, label='N=1000')
+plt.plot(sampling_time_1, square_wave_1, label='N=100')
+plt.plot(sampling_time_2, square_wave_2, label='N=1000')
 plt.xlabel("Tiempo [s]")
 plt.ylabel("Amplitud [V]")
 plt.legend()
