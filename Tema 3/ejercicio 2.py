5e69b8ee47fac87bd3f93627e8a2eec70f4534e0
Joel Jassan <joeljassan@hotmail.com>
2026-06-03 17:04:52 -0300

    ✨ feat: ajusta parámetros de señal y mejora la generación de ondas cuadradas

--- Tema 3/ejercicio 2.py
+++ Tema 3/ejercicio 2.py
@@ -10,36 +10,55 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Parámetros de la señal
-ciclos = 2
+ciclos = 3
 T = 1  # Período
-D = 0.5  # Ciclo activo del 50%
+D = 0.3  # Ciclo activo del 50%
 N1 = 100  # Número de muestras por período (caso 1)
 N2 = 1000  # Número de muestras por período (caso 2)
 
-#muestreo
+# Muestreo
 f_sampling_1 = N1
 f_sampling_2 = N2
 
-sampling_time_1 = np.arange(0, ciclos/T, 1/f_sampling_1)
-sampling_time_2 = np.arange(0, ciclos/T, 1/f_sampling_2)
+sampling_time_1 = np.arange(0, ciclos*T, 1/f_sampling_1)
+sampling_time_2 = np.arange(0, ciclos*T, 1/f_sampling_2)
 
 
-# Función para generar la señal discreta
+
+# Generación de señales discretas (onda cuadrada)
 square_wave_1 = []
-for i in sampling_time_1:
-    if i%T < D*T:
+for t in sampling_time_1:
+    tau = (t + T/2) % T - T/2 
+
+    if abs(tau) <= D*T/2: #centrado en la mitad del pulso activo
         square_wave_1.append(1)
     else:
         square_wave_1.append(0)
 square_wave_1 = np.array(square_wave_1)
 
 square_wave_2 = []
-for i in sampling_time_2:
-    if i%T < D*T:
+for t in sampling_time_2:
+    tau = (t + T/2) % T - T/2 
+
+    if abs(tau) <= D*T/2: 
         square_wave_2.append(1)
     else:
         square_wave_2.append(0)
-square_wave_1 = np.array(square_wave_1)
+square_wave_2 = np.array(square_wave_2)
+
+
+
+# ----- Fourier ------------------------- #
+from dataclasses import dataclass, field
+import math
+
+@dataclass
+class FourierCoefficients:
+    a0: float = 0.0
+    an: list = field(default_factory=list)
+    bn: list = field(default_factory=list)
+
+
 
 
 #visualización de señales
